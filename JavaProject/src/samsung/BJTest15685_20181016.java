b627f1f9b1fb5b305db2b2bd1dbc46edfa17ed9d
Roy <gsson@namyang-intl.com>
2018-10-18 09:43:54 +0900

    Merge됨

--- JavaProject/src/samsung/BJTest15685_20181016.java
+++ JavaProject/src/samsung/BJTest15685_20181016.java
@@ -1,10 +1,15 @@
 package samsung;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class BJTest15685_20181016 {
-	static int[][] square = new int[100][100];
+	static ArrayList<Node> list = new ArrayList<Node>();
+	static int[] wayX = {1,0,-1,0};
+	static int[] wayY = {0,-1,0,1};
+	static int[][] map = new int[101][101];
+	static int result=0;
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -17,39 +22,75 @@ public class BJTest15685_20181016 {
 			int y = Integer.parseInt(str[1]);
 			int way = Integer.parseInt(str[2]);
 			int circleNum = Integer.parseInt(str[3]);
+			
+			
 			int answer = solve(x, y, way, circleNum);
 		}
+		for(int p=0; p<list.size();p++) {
+			System.out.println(p+"는 list x : "+list.get(p).x + " list y : "+list.get(p).y);
+		}
+		
+
+		 for(int h=0; h<=100; h++){
+			 for(int v=0; v<=100; v++){
+				  if(map[h][v] ==1&& map[h][v+1] ==1&& map[h+1][v] ==1&& map[h+1][v+1]==1) {
+					  ++result;
+				  }
+			 }
+		 }
+		 System.out.println("정답은 : " + result);
+		
 	}
 	public static int solve(int x, int y, int way, int circleNum) {
-		Node node = new Node(x,y);
-		ArrayList<Node> list = new ArrayList<Node>();
-		list.add(node);
-		// 방향 전환
-		switch (way) {
-		case 0: x = x+1; break;
-		case 1: y = y-1; break;
-		case 2: x = x-1; break;
-		case 3: y = y+1; break;
-		}
-		list.add(new Node(x, y));
 		
-		for(int idx=0; idx<circleNum;idx++) {
-			int stdX =x;
-			int stdY =y;
-			System.out.println(Math.pow(2, circleNum-1));
-			for(int subIdx=0;subIdx<Math.pow(2, circleNum-1);subIdx++) {
-				int absX = Math.abs(stdX-list.get(subIdx).x);
-				int absY = Math.abs(stdY-list.get(subIdx).y);
-				list.add(new Node(x- absY, y- absX));
-			}
-		}
-		//기준점
-		for(int i=0; i<list.size();i++) {
-			System.out.println(i+"는 list x : "+list.get(i).x + " list y : "+list.get(i).y);
-		}
+		//방향 정보 리스트
+		ArrayList<Integer> wList = new ArrayList<Integer>();
+		// 스택이 담길 공간
+		LinkedList<Integer> stack = new LinkedList<Integer>();
+		
+		wList.add(way);
+		list.add(new Node(x,y));
+		map[x][y]=1;
+		// 넣는다.
+		logic(x,y,wList,stack, circleNum);
+		
 		return 0;
 	}
 	
+	public static void logic(int x, int y, ArrayList<Integer> wList, LinkedList<Integer> stack, int circleNum) {
+		
+		if(circleNum ==0) {
+			return;
+		}
+		
+		for(int m=0;m<wList.size();m++) {
+			// 이동할 circleNum을 찾고 이동한다. 그리고 넣는다.
+			stack.push(wList.get(m));
+			x = x+wayX[wList.get(m)];
+			y = y+wayY[wList.get(m)];
+			list.add(new Node(x,y));
+			map[x][y]=1;
+		}
+		
+		
+		for(int n=0;n<stack.size();n++) {
+			int newIdx;
+			int popNum = stack.pop()+1;
+			if(popNum%4 ==0) {
+				newIdx = 0;
+			} else {
+				newIdx = popNum;
+			}
+			wList.add(newIdx);
+			x = x+wayX[newIdx];
+			y = y+wayY[newIdx];
+			list.add(new Node(x,y));
+			map[x][y]=1;
+		}
+		logic(x,y,wList,stack,--circleNum);
+		
+	}
+	 
 	static class Node{
 		int x;
 		int y;
